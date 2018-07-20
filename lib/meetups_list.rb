0747f56f1dd074ab44acf820c2a0b6320a1b55f0
claytonflesher <claytonflesher@gmail.com>
2018-07-20 11:25:14 -0500

    more debugging:

--- lib/meetups_list.rb
+++ lib/meetups_list.rb
@@ -42,8 +42,9 @@ class MeetupsList
 
   def build_events(events)
     events.map do |event|
-      venue = build_venue(event) if event != nil
-      build_event(event, venue) if event != nil
+      puts event
+      venue = build_venue(event)
+      build_event(event, venue)
     end
   end
 
